6b08c15500da0b72a15a7070f7bff18e6b2d2984
Joseph Kim <josephkim570@gmail.com>
2020-03-30 14:01:28 -0400

    key to a solution was adding await in data and having a separate return in map of Issue component - stable working code

--- github_issues_ex/src/Components/Issue.js
+++ github_issues_ex/src/Components/Issue.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Issue.css';
-import IssueList from './IssueList';
 
-const Issue = ({ issue }) => {
+function Issue(props) {
+    let { title, url, body } = props;
     return (  
         <div className="Issues">
-            <h1>{issue}</h1>
-            <h1>{issue.title}</h1>
-            <h1>{issue.url}</h1>
-            <h1>{issue.body}</h1>
+            <p>{title}</p>
+            <a href={url}>{url}</a>
+            <p>{body}</p>
         </div>
     );
 };
--- github_issues_ex/src/Components/IssueList.js
+++ github_issues_ex/src/Components/IssueList.js
@@ -6,44 +6,44 @@ class IssueList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            issues: [],
-            title:'',
-            url:'',
-            body:''
+            issues: []
         };
     }
-
+    
     async componentDidMount() {
         try {
             const response = await fetch (`https://api.github.com/repos/facebook/create-react-app/issues`);
             const data = response.json();
             this.setState({
-                issues: data
+                issues: await data
             })
+            
             // Dev Test
             console.log(data);
+
         } catch(error) {
             console.log(error);
         }
     }
 
     render() { 
-        const { issues } = this.state;
-        const { title } = this.state;
-        const { url } = this.state;
-        const { body } = this.state;
+        let issuePopulate;
+        issuePopulate = this.state.issues.map((issue) => {
+            return <Issue 
+            title={issue.title} 
+            url={issue.url}
+            body={issue.body} 
+            />
+        })
     
         return (
             <div className="IssueList">
                 <ul>
-                    {issues.map(issue => (
-                        <Issue title= {issue.title} url= {issue.url} body= {issue.body}/>
-                    ))}
-                </ul>
-                
+                    {issuePopulate}
+                </ul> 
             </div>
-          );
-    }
+        );
+     }
 }
  
 export default IssueList;
